3b3688227fc30f3385326bf2e0ae74983f49e397
Mark Owen <mowen01@gmail.com>
2015-05-04 00:08:24 -0600

    Enhance testing with more html form tests while using a SimpleHTTPServer

--- tests/browser_test.py
+++ tests/browser_test.py
@@ -1,20 +1,39 @@
 import unittest
 import lxml.html as lh
+from octbrowser import __version__ as ob_version
 from octbrowser.browser import Browser
 
+import threading
+import SocketServer
+try:
+    # Python 2
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
+except ImportError:
+    # Python 3
+    from http.server import SimpleHTTPRequestHandler
+
+PORT = 8081
+BASE_URL = "http://localhost:{}".format(PORT)
+
+def start_http_server():
+    """
+    Run SimpleHTTPServer to serve files in test directory
+    """
+    httpd = SocketServer.TCPServer(("", PORT), SimpleHTTPRequestHandler)
+    t = threading.Thread(target=httpd.serve_forever)
+    t.setDaemon(True)
+    t.start()
+    return httpd
 
 class TestBrowserFunctions(unittest.TestCase):
 
     def setUp(self):
-        self.browser = Browser()
-
-        with open('html_test.html') as f:
-            self.html = f.read()
+        self.browser = Browser(base_url=BASE_URL)
 
     def test_form(self):
         """Test the form functions
         """
-        self.browser._html = lh.fromstring(self.html)
+        self.browser.open_url(BASE_URL + "/html_test.html")
         self.browser.get_form('.form')  # test with class selector
 
         # check input
@@ -27,6 +46,17 @@ class TestBrowserFunctions(unittest.TestCase):
 
         self.browser.get_form(None, nr=0)  # test with nr param
         self.assertEqual(self.browser.form_data['test'], 'OK')
+
+        # Check data and headers of submit_form
+        user_agent = 'python-octbrowser/{}'.format(ob_version)
+        self.browser.add_header('User-Agent', user_agent)
+        self.browser.form_data['test'] = 'octbrowser'
+        r = self.browser.submit_form()
+
+        # Verify user-agent header
+        self.assertTrue('User-Agent' in r.request.headers,
+                        'User-Agent is in post headers')
+        self.assertEqual(r.request.headers['User-Agent'], user_agent)
 
     def test_navigation(self):
         """Testing history
@@ -57,7 +87,7 @@ class TestBrowserFunctions(unittest.TestCase):
     def test_get_elements(self):
         """Testing the get_html_elements method
         """
-        self.browser._html = lh.fromstring(self.html)
+        self.browser.open_url(BASE_URL + "/html_test.html")
 
         # get the single p tag
         p = self.browser.get_html_element('#myparaf')
@@ -70,4 +100,8 @@ class TestBrowserFunctions(unittest.TestCase):
         self.assertTrue(len(tags) == 4)
 
 if __name__ == '__main__':
-    unittest.main()
+    httpd = start_http_server()
+    try:
+        unittest.main()
+    finally:
+        httpd.server_close()
